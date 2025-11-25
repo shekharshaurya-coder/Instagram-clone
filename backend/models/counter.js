d77bcb02d381b343c0a94217178aec78205ce7ae
Shaurya Shekhar <ershekhr@gmail.com>
2025-11-25 15:22:13 +0530

    deleted models to upload now models

--- backend/models/counter.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const CounterSchema = new Schema({
-  name: { type: String, required: true, unique: true },
-  value: { type: Number, default: 0 },
-});
-
-module.exports = model("Counter", CounterSchema);
